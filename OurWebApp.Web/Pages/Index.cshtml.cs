24d843339bbe049aeedd15cb7f17238e6293f8c9
azureclouddevlab <azureclouddevlab@gmail.com>
2020-06-17 23:39:24 -0700

    initial add

---
+++ OurWebApp.Web/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace OurWebApp.Web.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IConfiguration Config;
+
+        public string KeyVaultName
+        {
+            get
+            {
+                return Config.GetValue<string>("KeyVaultName");
+            }
+        }
+
+        // Constructor that that takes IConfiguration is called on instantiation thanks to Dependency injection
+        public IndexModel(IConfiguration config)
+        {
+            Config = config;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
